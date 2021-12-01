14dd462802cfb3602fb52fd161f9d6ac28534ff2
Sun mengzhou <50372596+mengzhou-sun@users.noreply.github.com>
2021-12-01 08:11:21 +0800

    Add files via upload

---
+++ CRNN and Seq2seq/CRNN/String Transformation.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+from collections import defaultdict
+import torch as t
+import numpy as np
+import mediapipe as my
+
+def labelDictionary():
+    labels = [' ', '!', '"', '#', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    letter2index = {label: n for n, label in enumerate(labels)}
+    index2letter = {v: k for k, v in letter2index.items()}
+    return len(labels), letter2index, index2letter
+
+num_classes, letter2index, index2letter = labelDictionary()
+tokens = {'GO_TOKEN': 0, 'END_TOKEN': 1, 'PAD_TOKEN': 2}
+num_tokens = len(tokens.keys())
+print(num_classes+num_tokens)
+a=[0,37,86,7,87,67,72,62,7,34,79,76,65,63,76,71,63,67,87,78,63,76,7,81,73,76,62,63,72,7,66,63,76,7,74,63,78,63,76,7,66,59,70,70,63,76,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+print(',,,,',len(a))
